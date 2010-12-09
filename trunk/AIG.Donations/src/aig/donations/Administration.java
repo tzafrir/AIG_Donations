2302f21406495532f03b7980ffe9df3a5688b392
s3 <s3@ef6e11f1-5243-43a2-aaa6-696aa70d755e>
2010-12-09 19:59:42 +0000

    changed some small stuff in Administration
    git-svn-id: https://ssdl-linux.cs.technion.ac.il/cs234311-svn/yp2010-g3/AIG.Donations@178 ef6e11f1-5243-43a2-aaa6-696aa70d755e

--- trunk/AIG.Donations/src/aig/donations/Administration.java
+++ trunk/AIG.Donations/src/aig/donations/Administration.java
@@ -1,5 +1,6 @@
 package aig.donations;
 
+import aig.donations.exceptions.BadLoginException;
 import aig.donations.exceptions.IllegalPasswordException;
 import aig.donations.exceptions.IllegalPasswordLengthException;
 import aig.donations.exceptions.UserAlreadyExistsException;
@@ -17,36 +18,18 @@ class Administration {
     UserDBHandler.addToDB(username, role, name, password);
   }
   
-  static boolean login(String username, String password) {
+  static User login(String username, String password) throws BadLoginException {
     String realPassword = null;
     try {
       realPassword = UserDBHandler.retrievePassword(username);
     } catch (UserNotFoundException e) {
-      return false; // bad user
+      throw new BadLoginException(); // bad user
     }
     if (null == realPassword || !realPassword.equals(password)) {
-      return false; // bad password
+      throw new BadLoginException(); // bad password
     }
     
-    User user = UserDBHandler.retrieveUser(username);
-    
-    saveUserInSession(user);
-    
-    return true;
-  }
-  
-  static void logout() {
-    removeUserFromSession();
-  }
-  
-  private static void saveUserInSession(User user) {
-    // TODO Auto-generated method stub
-    // TODO: if a User already exists in the session, replace it (or throw?)
-  }
-  
-  private static void removeUserFromSession() {
-    // TODO Auto-generated method stub
-    // TODO: throw if no User in the session
+    return UserDBHandler.retrieveUser(username);
   }
   
 }
---
+++ trunk/AIG.Donations/src/aig/donations/exceptions/BadLoginException.java
@@ -0,0 +1,10 @@
+package aig.donations.exceptions;
+
+public class BadLoginException extends Exception {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  
+}
