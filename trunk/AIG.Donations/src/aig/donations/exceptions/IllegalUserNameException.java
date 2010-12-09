4f83112422ec24c89cf17d794c007abe65a58914
s3 <s3@ef6e11f1-5243-43a2-aaa6-696aa70d755e>
2010-12-09 13:18:06 +0000

    small bug in the commit... (2)
    git-svn-id: https://ssdl-linux.cs.technion.ac.il/cs234311-svn/yp2010-g3/AIG.Donations@172 ef6e11f1-5243-43a2-aaa6-696aa70d755e

---
+++ trunk/AIG.Donations/src/aig/donations/exceptions/IllegalUserNameException.java
@@ -0,0 +1,14 @@
+package aig.donations.exceptions;
+
+public class IllegalUserNameException extends IllegalArgumentException {
+  
+  public IllegalUserNameException(String message) {
+    super(message);
+  }
+  
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  
+}
---
+++ trunk/AIG.Donations/src/aig/donations/exceptions/IllegalUserNameLengthException.java
@@ -0,0 +1,14 @@
+package aig.donations.exceptions;
+
+public class IllegalUserNameLengthException extends IllegalUserNameException {
+  
+  public IllegalUserNameLengthException(int min, int max) {
+    super("name length should be between " + min + " and " + max);
+  }
+  
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  
+}
